d4753663d094b6399c63b39117312bdfd3f77462
Sebastian Diaz Jimenez <sebasdj2006@gmail.com>
2024-02-28 12:30:33 -0500

    Update clase4.js

--- JavaScript/clase4/clase4.js
+++ JavaScript/clase4/clase4.js
@@ -4,4 +4,18 @@ let numeros =[10.34,1,100.312]
 const varios =["Juan",true,10,null,3.0]
 
 console.log(numeros);
-console.log(varios);
+console.log(varios[0]); // se pone eso para imprimir solo el que queramos 
+console.log(varios.length);
+
+//agrego al final
+numeros.push("Colombia");
+console.log(numeros);
+//quito el ultimo
+numeros.pop();
+
+
+//poner al inicio
+numeros.unshift("Brasil");
+//quitar el primero 
+numeros.shift();
+console.log(numeros);
