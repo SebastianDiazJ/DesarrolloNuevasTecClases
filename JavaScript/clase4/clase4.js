cdefb5c996d05a2ef53239b3d9490c33ac1ef1fd
Sebastian Diaz Jimenez <sebasdj2006@gmail.com>
2024-03-05 12:13:37 -0500

    tarea

--- JavaScript/clase4/clase4.js
+++ JavaScript/clase4/clase4.js
@@ -81,7 +81,7 @@ const arreglo2 = ["s", "e", "b", "a", "e","k"];
 const arreglo3 = [true, false, true, true, false];
 const arreglo4 = [10, 20, 15.7, 30, 25];
 
-// Métodos de arreglo y forEach
+// metodos de arreglo y forEach
 console.log("Arreglo 1:");
 
 // filter para obtener  los números pare
@@ -94,12 +94,12 @@ const mayusculas = arreglo2.map(letra => letra.toUpperCase());
 console.log("Mayúsculas:", mayusculas);
 
 console.log("Arreglo 3:");
-// Método reduce para contar cuántos elementos son falsos
+// metodo reduce para contar cuántos elementos son falsos
 const contarFalsos = arreglo3.reduce((contador, valor) => contador + (valor === false ? 1 : 0), 0);
 console.log("Cantidad de falsos:", contarFalsos);
 
 console.log("Arreglo 4:");
-// Método forEach para el arreglo4 elevado a 2
+// metodo forEach para el arreglo elevado a 2
 arreglo4.forEach(num => console.log(num ** 2));
 
 
